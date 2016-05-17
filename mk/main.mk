44a90cb1e1cb27c279558e5d0f8725c09665c4b9
kemar <marc.hertzog@polyconseil.fr>
2016-05-17 14:26:29 +0200

    Revert "Fix the path of `gettext-extract` and `gettext-compile` binaries"
    This reverts commit b01b2ceeb712de622ba8e45975235b6f1b710d7b.

--- mk/main.mk
+++ mk/main.mk
@@ -165,7 +165,7 @@ endif
 /tmp/template.pot: $(GETTEXT_JS_SOURCES) $(GETTEXT_HTML_SOURCES)
 	@echo "$(ECHOPREFIX) extracting translations $(ECHOSUFFIX)"
 	mkdir -p $(dir $@)
-	$(NODE_BINDIR)gettext-extract --output $@ $(GETTEXT_HTML_SOURCES)
+	gettext-extract --output $@ $(GETTEXT_HTML_SOURCES)
 	xgettext --language=JavaScript --keyword=i18n --from-code=utf-8 \
 		--sort-output --join-existing --no-wrap --package-name=$(PACKAGE_NAME) \
 		--package-version=$(shell node -e "console.log(require('./package.json').version);") \
@@ -182,4 +182,4 @@ endif
 
 $(OUTPUT_DIR)/translations.json: /tmp/template.pot
 	mkdir -p $(OUTPUT_DIR)
-	$(NODE_BINDIR)gettext-compile --output $@ $(LOCALE_FILES)
+	gettext-compile --output $@ $(LOCALE_FILES)
