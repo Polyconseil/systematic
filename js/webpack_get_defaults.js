d6c84750a97c013e4abd7ab042277de2ea3efa82
Stéphane Raimbault <stephane.raimbault@polyconseil.fr>
2016-06-09 19:10:26 +0200

    Fix URL resolving of glyph icons in dist bundle by separating CSS

--- js/webpack_get_defaults.js
+++ js/webpack_get_defaults.js
@@ -8,12 +8,13 @@ const path = require('path')
 // TODO(vperron): Terrible to require this for the 5 lines this plugin is.
 const combineLoaders = require('webpack-combine-loaders')
 const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = require('./config')
 const enums = require('./config_choices')
 
 /* Global plugins collection, will be filled according to the profile */
-const plugins = []
+const plugins = [new ExtractTextPlugin('bundle.css')]
 
 /* Pre-configure loaders */
 const jsLoaders = [{
@@ -32,10 +33,9 @@ const cssLoader = {
 }
 const postcssLoader = { loader: 'postcss', query: {} }
 const sassLoader = { loader: 'sass', query: {} }
-const styleLoader = { loader: 'style', query: {} }
 
-const cssLoaders = [styleLoader, cssLoader, postcssLoader]
-const sassLoaders = [styleLoader, cssLoader, postcssLoader, sassLoader]
+const cssLoaders = [cssLoader, postcssLoader]
+const sassLoaders = [cssLoader, postcssLoader, sassLoader]
 
 const PRODUCTION_MODE = (process.env.SYSTEMATIC_BUILD_MODE === 'PROD')
 
@@ -115,8 +115,8 @@ module.exports = function (basePath) {
           include: [PATHS.src],
 
         },
-        { test: /\.css/, loader: combineLoaders(cssLoaders) },
-        { test: /\.scss$/, loader: combineLoaders(sassLoaders) },
+        { test: /\.css/, loader: ExtractTextPlugin.extract(combineLoaders(cssLoaders)) },
+        { test: /\.scss$/, loader: ExtractTextPlugin.extract(combineLoaders(sassLoaders)) },
         { test: /\.jade$/, loader: 'jade' },
         { test: /\.html$/, loader: 'html' },
         { test: /\.json$/, loader: 'json' },
