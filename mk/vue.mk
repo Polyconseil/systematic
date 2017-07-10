09ca3d8c2f575b9400e06a8abe641838d5feff10
yunshi <yunshi.tan@polyconseil.fr>
2017-07-10 08:34:42 +0200

    mk: includes .vue in gettext sources

--- mk/vue.mk
+++ mk/vue.mk
@@ -1,2 +1,5 @@
+GETTEXT_HTML_SOURCES ?= $(shell find $(SRC_DIR) -name '*.jade' -o -name '*.html' -o -name '*.vue' 2> /dev/null)
+GETTEXT_JS_SOURCES   ?= $(shell find $(SRC_DIR) -name '*.js' -o -name '*.vue')
+
 GETTEXTEXTRACT_OPTIONS := --attribute='v-translate'
 ESLINTOPTIONS := --ext='.js' --ext='.vue' --plugin='html'
