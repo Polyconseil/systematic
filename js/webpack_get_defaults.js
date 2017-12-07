Polyconseil/systematic

commit cfaecf1e5c8955a197df9c9fb3726dfb4ed100eb

Production: use custom uglifyJS settings.
We want to fine-tune the UglifyJS settings since those mauy slightly
change the application logic; we have to have our hand over it.

js/webpack_get_defaults.js
@@ -7,6 +7,7 @@ const path = require('path')
 
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
 
 const config = require('./config')
 const enums = require('./config_choices')
@@ -203,6 +204,49 @@ module.exports = function (basePath) {
   if (PRODUCTION_MODE) {
     const extractCSS = new ExtractTextPlugin(getOutputCssFileName())
     plugins.push(extractCSS)
+    const uglifyJS = new UglifyPlugin({
+      compress: {
+        angular       : false,
+        booleans      : false,  // not the default
+        cascade       : true,
+        collapse_vars : true,
+        comparisons   : false,  // not the default
+        conditionals  : false,  // not the default
+        dead_code     : true,
+        drop_console  : false,
+        drop_debugger : true,
+        evaluate      : true,
+        expression    : false,
+        global_defs   : {},
+        hoist_funs    : true,
+        hoist_vars    : false,
+        if_return     : false,  // not the default
+        join_vars     : true,
+        keep_fargs    : true,
+        keep_fnames   : false,
+        keep_infinity : false,
+        loops         : true,
+        negate_iife   : true,
+        passes        : 1,
+        properties    : true,
+        pure_getters  : true && "strict",
+        pure_funcs    : null,
+        reduce_vars   : true,
+        screw_ie8     : true,
+        sequences     : true,
+        side_effects  : true,
+        switches      : true,
+        top_retain    : null,
+        unsafe        : false,
+        unsafe_comps  : false,
+        unsafe_math   : false,
+        unsafe_proto  : false,
+        unsafe_regexp : false,
+        unused        : true,
+        warnings      : true,
+      }
+    })
+    plugins.push(uglifyJS)
     cssRulesAggregator = function (loaders) {
       return extractCSS.extract({
         use: loaders,

mk/main.mk
@@ -166,7 +166,7 @@ serve: prepare
 dist: prepare
 	mkdir -p $(OUTPUT_DIR)
 	NODE_ENV=production webpack $(WEBPACK_OPTIONS_CONFIG_FILE) \
-		--no-color --bail --display-modules  -p \
+		--no-color --bail --display-modules \
 		$(WEBPACK_DIST_OPTS) $(WEBPACK_EXTRA_OPTIONS)
 
 # Miscellaneous build commands

commit 2ade156e611f6ec1113a2d5479d7c9dc575a36de

webpack: use DefinePlugin to pass NODE_ENV to Uglify
This commit fixes a regression introduced by cfaecf1e5c8955a197.

According to the webpack doc:

-p is equivalent to --optimize-minimize --define process.env.NODE_ENV="production"

Removing the -p had the effect of preventing UglifyJS from removing Vuejs
dev mode logging. We for example had the following log in our production
bundles:

    You are running Vue in development mode.
    Make sure to turn on production mode when deploying for production.
    See more tips at https://vuejs.org/guide/deployment.html

The --define process.env.NODE_ENV="production" can be replaced by this
commit (as advised by the vuejs official documentation).

js/webpack_get_defaults.js
@@ -5,6 +5,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const webpack = require('webpack')
+
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyPlugin = require('uglifyjs-webpack-plugin')
@@ -240,6 +242,7 @@ module.exports = function (basePath) {
         use: loaders,
       })
     }
+    plugins.push(new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}))
   }
 
   switch (config.build.type) {