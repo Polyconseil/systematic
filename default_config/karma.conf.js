dda2d57fb063541b1ba2de27268108f127da9fef
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-03-19 22:18:30 +0100

    default_config: use webpack.conf.js as default for karma

--- default_config/karma.conf.js
+++ default_config/karma.conf.js
@@ -1,5 +1,11 @@
 const projectPath = require('./project_path')
-const karmaDefaults = require('systematic').karma_get_defaults(projectPath)
+const fs = require('fs')
+const webpackConfigDefaultPath = projectPath + '/webpack.config.js'
+let webpackConfig = null
+if (fs.existsSync(webpackConfigDefaultPath)) {
+  webpackConfig = require(webpackConfigDefaultPath)
+}
+const karmaDefaults = require('systematic').karma_get_defaults(projectPath, webpackConfig)
 
 // optional overrides
 
